447de485267609ef965f7c536700365502a04d83
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2026-03-09 11:13:36 +0000

    fix mmcblk boot

--- src/scripts/filesystem.sh
+++ src/scripts/filesystem.sh
@@ -14,7 +14,7 @@ function get_part(){
 }
 
 function init_top(){
-    for mod in sd_mod sr_mod nvme usb-storage ; do
+    for mod in mmc_block sd_mod sr_mod nvme usb-storage ; do
         modprobe $mod || true
     done
     if [ "${root#UUID=}" != "$root" ]; then
