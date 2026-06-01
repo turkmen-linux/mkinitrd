9e548ba7afd24a8fef46d57068c58b1a25fab255
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2026-06-01 11:01:11 +0000

    bind mount to hide boot_id

--- src/scripts/privacy.sh
+++ src/scripts/privacy.sh
@@ -4,6 +4,9 @@ init_top(){
 
 init_bottom(){
     # hide hardware info
-    mount -t tmpfs -o ro tmpfs /sys/class/dmi
-    mount -t tmpfs -o ro tmpfs /sys/devices/virtual/dmi
+    mount --make-private -t tmpfs -o ro tmpfs /sys/class/dmi || true
+    mount --make-private -t tmpfs -o ro tmpfs /sys/devices/virtual/dmi || true
+    # hide boot_id
+    mount --make-private --bind /proc/sys/kernel/random/uuid \
+        /proc/sys/kernel/random/boot_id || true
 }
